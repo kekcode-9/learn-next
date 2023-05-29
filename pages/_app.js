f6f353eee1606fe5e031e8380977f9e90e3c50f3
kekcode-9 <rupamita.s97@gmail.com>
2023-05-29 17:15:53 +0530

    Next-main initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import App from 'next/app'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
@@ -5,3 +6,9 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const pageProps = await App.getInitialProps(appContext);
+  return { ...pageProps };
+}
